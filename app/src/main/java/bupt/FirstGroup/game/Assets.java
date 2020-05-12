d13f57480d1dc2111eea187e261b63fcc7078d54
Carolwing <1251503654@qq.com>
2020-05-13 00:10:21 +0800

    首次提交

---
+++ app/src/main/java/bupt/FirstGroup/game/Assets.java
@@ -0,0 +1,28 @@
+package bupt.FirstGroup.game;
+
+import bupt.FirstGroup.framework.Image;
+import bupt.FirstGroup.framework.Music;
+import bupt.FirstGroup.framework.Sound;
+
+class Assets {
+    // Graphics
+    static Image background;
+    static Image gameover;
+    static Image pause;
+    static Image ballNormal;
+    static Image ballMultiplier;
+    static Image ballOneUp;
+    static Image ballSpeeder;
+    static Image ballBomb;
+    static Image explosion;
+    static Image ballSkull;
+    static Image explosionBright;
+    static Image sirens;
+
+    // Audio
+    static Sound soundClick;
+    static Sound soundExplosion;
+    static Sound soundCreepyLaugh;
+
+    static Music musicTrack;
+}
