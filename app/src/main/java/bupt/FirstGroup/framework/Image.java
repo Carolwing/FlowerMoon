75ab7d4bbd6b58b35a58a1483012472cc4df7c49
Carolwing <1251503654@qq.com>
2020-05-30 15:02:02 +0800

    调节图片的大小

--- app/src/main/java/bupt/FirstGroup/framework/Image.java
+++ app/src/main/java/bupt/FirstGroup/framework/Image.java
@@ -1,8 +1,12 @@
 package bupt.FirstGroup.framework;
 
+import android.graphics.Bitmap;
+
 public interface Image {
     public int getWidth();
     public int getHeight();
     public Graphics.ImageFormat getFormat();
     public void dispose();
+    public Image resetSize(int _width, int _height);
+    public boolean getIsrest();
 }
