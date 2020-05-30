a67e28e1b5dab75b151e173578db7b56707c7cb6
Carolwing <1251503654@qq.com>
2020-05-30 15:02:45 +0800

    实现随着曲谱生成note

--- app/src/main/java/bupt/FirstGroup/game/models/Ball.java
+++ app/src/main/java/bupt/FirstGroup/game/models/Ball.java
@@ -1,23 +1,31 @@
 package bupt.FirstGroup.game.models;
 
 public class Ball {
-    public enum BallType {
-        Normal, OneUp, Multiplier, Speeder, Bomb, Skull
-    }
+
 
     public int x;
     public int y;
-    public BallType type;
+    public int type;
     private double speedMultiplier;
+    private float time;
 
-    public Ball(int x, int y, BallType type){
+    public Ball(int x, int y, int type, float time){
         this.x = x;
         this.y = y;
         this.type = type;
-        this.speedMultiplier = type == BallType.Speeder ? 1.4 : 1;
+//        this.speedMultiplier = type == BallType.Speeder ? 1.4 : 1;
+        this.time=time;
     }
 
     public void update(int speed) {
-        this.y += speed * speedMultiplier;
+        this.y += speed;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public int getType() {
+        return type;
     }
 }
